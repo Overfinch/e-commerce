16c48dce1c73273126a51fae4f8c79217f7c44c0
Overfinch <bundichmail@gmail.com>
2018-06-26 19:58:37 +0300

    Starting the landing page

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Product::create([
+            'name' => 'Laptop 1',
+            'slug' => 'Laptop-1',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 2',
+            'slug' => 'Laptop-2',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 3',
+            'slug' => 'Laptop-3',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 4',
+            'slug' => 'Laptop-4',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 5',
+            'slug' => 'Laptop-5',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 6',
+            'slug' => 'Laptop-6',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 7',
+            'slug' => 'Laptop-7',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 8',
+            'slug' => 'Laptop-8',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+
+        \App\Product::create([
+            'name' => 'Laptop 9',
+            'slug' => 'Laptop-9',
+            'details' => '15 inch, core i7, 2tb ssd',
+            'price' => 14999,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales sem eu posuere auctor. Quisque dapibus, purus id mattis aliquam, arcu diam rutrum turpis, et bibendum lorem erat quis felis. Morbi id nibh nec tellus scelerisque scelerisque a non purus. Integer fringilla nibh turpis, in tempus nibh fermentum sit amet. Fusce pulvinar hendrerit tincidunt. Duis ultrices maximus scelerisque. Nullam et turpis venenatis, lobortis mi vitae, tristique nibh.',
+        ]);
+    }
+}
