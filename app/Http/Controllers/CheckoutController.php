8c71aea5a7876758e84bbb5a73bbf1c3e3281a60
Overfinch <bundichmail@gmail.com>
2018-08-14 16:04:19 +0300

    add CardErrorException

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Cartalyst\Stripe\Exception\CardErrorException;
 use Cartalyst\Stripe\Laravel\Facades\Stripe;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
@@ -14,11 +15,11 @@ class CheckoutController extends Controller
 
     public function store(Request $request){
         try{
-            $contents = Cart::instance('default')->content()->map(function ($item){
+            $contents = Cart::instance('default')->content()->map(function ($item){ // собираем json с данными про товары в корзине что бы передать его в stripe
                     return $item->model->slug.', '.$item->qty;
                 })->values()->toJson();
 
-            $charge = Stripe::charges()->create([
+            $charge = Stripe::charges()->create([ // создаём и передаём в stripe данные про заказ
                 'amount' => Cart::instance('default')->total() / 100,
                 'currency' => 'UAH',
                 'source' => $request->stripeToken,
@@ -29,10 +30,11 @@ class CheckoutController extends Controller
                     'quatity' => Cart::instance('default')->count(),
                 ],
             ]);
-            Cart::instance('default')->destroy();
-            return redirect()->route('confirmation.index')->with('success_message','Thank you! Your payment was successfully accepted!');
-        } catch (\Exception $e){
 
+            Cart::instance('default')->destroy(); // удаляем содержимое корзины
+            return redirect()->route('confirmation.index')->with('success_message','Thank you! Your payment was successfully accepted!'); // обязательно передаём success_message
+        } catch (CardErrorException $e){
+            return back()->withErrors('Error',$e->getMessage());
         }
     }
 }
